64ebec4fe7b48c131b7d6f0f8d7a6c6cacae70e6
Artyom Kazak <yom@artyom.me>
2018-06-26 17:08:24 +0200

    Allow overriding the connection per 'Request'

--- http-client/Network/HTTP/Client/Request.hs
+++ http-client/Network/HTTP/Client/Request.hs
@@ -287,6 +287,7 @@ defaultRequest = Request
                 Just (_ :: IOException) -> return ()
                 Nothing -> throwIO se
         , requestManagerOverride = Nothing
+        , connectionOverride = Nothing
         }
 
 -- | Parses a URL via 'parseRequest_'
